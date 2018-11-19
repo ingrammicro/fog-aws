900685edf371f39e23f7865de2d3e26922a0e492
Samuel Patino <samuelp@softcom.com>
2018-11-19 16:31:58 +0100

    Add VPN gateway and customer gateway

---
+++ lib/fog/aws/models/compute/vpn_gateway.rb
@@ -0,0 +1,69 @@
+module Fog
+  module AWS
+    class Compute
+        class VpnGateway < Fog::Model
+          identity  :vpn_gateway_id,                :aliases => 'vpnGatewayId'
+          attribute :amazon_side_asn,               :aliases => 'amazonSideAsn'
+          attribute :attachments,                   :aliases => 'attachments'
+          attribute :availability_zone,             :aliases => 'availabilityZone'
+          attribute :state,                         :aliases => 'state'
+          attribute :tag_set,                       :aliases => 'tagSet'
+          attribute :type,                          :aliases => 'type'
+
+          def ready?
+            state == 'available'
+          end
+  
+          # Removes an existing VPN gateway
+          #
+          # vpn_gateway.destroy
+          #
+          # ==== Returns
+          #
+          # True or false depending on the result
+          #
+          def destroy
+            requires :vpn_gateway_id
+  
+            service.delete_vpn_gateway(vpn_gateway_id)
+            true
+          end
+  
+          # Create a VPN gateway
+          #
+          #  >> g = AWS.vpn_gateways.new(type: "ipsec.1", options)
+          #  >> g.save
+          #
+          # == Returns:
+          #
+          # requestId and a vpnGateway object
+          #
+          def save
+            requires :type
+            options = {
+              'AmazonSideAsn'     => amazon_side_asn,
+              'AvailabilityZone'  => availability_zone
+            }
+            options.delete_if {|key, value| value.nil?}
+            data = service.create_vpn_gateway(type, options).body['vpnGateway']
+            new_attributes = data.reject {|key,value| key == 'requestId'}
+            merge_attributes(new_attributes)
+            true
+          end
+
+          def attach(vpc_id)
+            requires :vpn_gateway_id
+            service.attach_vpn_gateway(vpc_id, vpn_gateway_id)
+            reload
+          end
+
+          def detach(vpc_id)
+            requires :vpn_gateway_id
+            service.detach_vpn_gateway(vpc_id, vpn_gateway_id)
+            reload
+          end
+        end
+      end
+    end
+  end
+  
---
+++ lib/fog/aws/requests/compute/delete_customer_gateway.rb
@@ -0,0 +1,34 @@
+module Fog
+  module AWS
+    class Compute
+      class Real
+        require 'fog/aws/parsers/compute/basic'
+        # Deletes a customer gateway.
+        #
+        # ==== Parameters
+        # * customer_gateway_id<~String> - The ID of the customer gateway you want to delete.
+        #
+        # === Returns
+        # * response<~Excon::Response>:
+        # * body<~Hash>:
+        # * 'requestId'<~String> - Id of request
+        # * 'return'<~Boolean> - Returns true if the request succeeds.
+        #
+        # {Amazon API Reference}[http://docs.amazonwebservices.com/AWSEC2/2012-03-01/APIReference/ApiReference-query-DeleteCustomerGateway.html]
+        def delete_customer_gateway(customer_gateway_id)
+          request(
+            'Action'            => 'DeleteCustomerGateway',
+            'CustomerGatewayId' => customer_gateway_id,
+            :parser => Fog::Parsers::Compute::AWS::Basic.new
+          )
+        end
+      end
+
+      class Mock
+        def delete_customer_gateway(customer_gateway_id)
+          raise "Not yet implemented"
+        end
+      end
+    end
+  end
+end
